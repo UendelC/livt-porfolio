229f2061a2b67774521f0608aeba209d6f3687e8
uendel.couto <uendel.couto@gmail.com>
2022-09-12 22:11:58 -0300

    feat: initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected array $fillable = [
+        'skill_id',
+        'name',
+        'image',
+        'url',
+    ];
+
+    public function skill(): BelongsTo
+    {
+        return $this->belongsTo(Skill::class);
+    }
+}
